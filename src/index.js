56b3ca3ec467fb15b7cb58f6ca02ba026e6f47ef
Talent Zeng <tzeng@sandvine.com>
2017-11-05 17:16:30 -0500

    added video files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 //API Key from Youtube
 const API_KEY = 'AIzaSyBgRf8J6zTHJsyF1LbbMtu42eJBH3krlqU';
 
 //Create a new component, this will be used to creat some HTML
-const App = () => {
-	return <div>Hello World!</div>;
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			//Note: this is equivalent to this.setState({ videos: videos})
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={ this.state.videos }/>
+			</div>
+		);
+	}
 }
 
 //Take this component and render it in the DOM
